d3edf440296f0d6e3a9b9f89ffac478f0eb20b4b
Luan Tran <developedbyluan@gmail.com>
2025-07-18 22:48:54 +0700

    expose hidden CodingKeys

--- FlucomDex/FetchedPokemon.swift
+++ FlucomDex/FetchedPokemon.swift
@@ -19,4 +19,33 @@ struct FetchedPokemon: Decodable {
     let speed: Int16
     let specialAttack: Int16
     let specialDefense: Int16
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case types
+        case sprite
+        case shiny
+        case attack
+        case defense
+        case hp
+        case speed
+        case specialAttack
+        case specialDefense
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int16.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.types = try container.decode([String].self, forKey: .types)
+        self.sprite = try container.decode(URL.self, forKey: .sprite)
+        self.shiny = try container.decode(URL.self, forKey: .shiny)
+        self.attack = try container.decode(Int16.self, forKey: .attack)
+        self.defense = try container.decode(Int16.self, forKey: .defense)
+        self.hp = try container.decode(Int16.self, forKey: .hp)
+        self.speed = try container.decode(Int16.self, forKey: .speed)
+        self.specialAttack = try container.decode(Int16.self, forKey: .specialAttack)
+        self.specialDefense = try container.decode(Int16.self, forKey: .specialDefense)
+    }
 }
