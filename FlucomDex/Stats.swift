89c0fb087857782ca5622ee1975106c95531aa52
Luan Tran <developedbyluan@gmail.com>
2025-07-20 10:09:46 +0700

    start building Stats View

---
+++ FlucomDex/Stats.swift
@@ -0,0 +1,20 @@
+//
+//  Stats.swift
+//  FlucomDex
+//
+//  Created by Tran Luan on 20/7/25.
+//
+
+import SwiftUI
+
+struct Stats: View {
+    var pokemon: Pokemon
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Stats(pokemon: PersistenceController.previewPokemon)
+}
