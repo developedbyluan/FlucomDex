9cbe0167fe373ca17cd564f399c08c6d06f0b41c
Luan Tran <developedbyluan@gmail.com>
2025-07-20 09:03:37 +0700

    start building PokemonDetail View

--- FlucomDex/PokemonDetail.swift
+++ FlucomDex/PokemonDetail.swift
@@ -15,10 +15,65 @@ struct PokemonDetail: View {
     @State private var showShiny: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack {
+                Image(.normalgrasselectricpoisonfairy)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black, radius: 6)
+                
+                AsyncImage(url: pokemon.sprite) { image in
+                    image
+                        .interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 80)
+                        .shadow(color: .black, radius: 6)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+            
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                        .shadow(color: .white, radius: 1)
+                        .padding(.vertical, 7)
+                        .padding(.horizontal)
+                        .background(Color(type.capitalized))
+                        .clipShape(.capsule)
+                    
+                }
+                
+                Spacer()
+                
+                Button {
+                    pokemon.favorite.toggle()
+                    
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        print(error)
+                    }
+                    
+                } label: {
+                    Image(systemName: pokemon.favorite ? "star.fill" : "star")
+                        .font(.largeTitle)
+                        .tint(.yellow)
+                }
+            }
+            .padding()
+            
+        }
+        .navigationTitle(pokemon.name!.capitalized)
     }
 }
 
 #Preview {
-    PokemonDetail().environmentObject(PersistenceController.previewPokemon)
+    NavigationStack {
+        PokemonDetail().environmentObject(PersistenceController.previewPokemon)
+    }
 }
